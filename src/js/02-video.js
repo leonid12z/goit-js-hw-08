9d3503a49da1c7d15aca5736e50e6621ff775740
leonid12z <lelik_ceh@ukr.net>
2022-02-16 20:30:56 +0200

    add+files+08d/z

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle'; 
+
+const STORAGE_VIDEO_PLAYER = 'videoplayer-current-time';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+function onTimeData(data) {
+   localStorage.setItem(STORAGE_VIDEO_PLAYER, data.seconds);
+};
+
+player.on('timeupdate', throttle(onTimeData, 1000));
+
+const currentTimePlay = localStorage.getItem(STORAGE_VIDEO_PLAYER);
+   if (currentTimePlay) {
+      player.setCurrentTime(currentTimePlay);
+}
+
+player.getVideoTitle().then(function(title) {
+   console.log('title:', title);
+});
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,57 @@
+import throttle from 'lodash.throttle';
+
+const STORAGE_KEY = 'feedback-form-state';
+
+const refs = {
+   form: document.querySelector('.feedback-form'),
+   input: document.querySelector('.feedback-form input'),
+   textarea: document.querySelector('textarea'),
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onFormInput, 500));
+
+let formData = {
+   email: '',
+   message: '',
+};
+
+populateFeedbackFrom();
+
+//  * - Останавливаем поведение по умолчанию
+//  * - Убираем данные из хранилища
+//  * - Очищаем форму
+
+function onFormSubmit(evet) {
+   evet.preventDefault();
+   evet.currentTarget.reset();
+
+   console.log(formData);
+
+   localStorage.removeItem(STORAGE_KEY);
+}
+
+//  * - Получаем значение поля
+//  * - Сохраняем его в хранилище
+//  * - Можно добавить throttle
+
+function onFormInput(evet) {
+   formData[evet.target.name] = evet.target.value;
+   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+//  * - Получаем значение из хранилища
+//  * - Если там что-то было, обновляем DOM
+
+function populateFeedbackFrom() {
+
+   const savedMessage = localStorage.getItem(STORAGE_KEY);
+
+   if (savedMessage) {
+
+      formData = JSON.parse(savedMessage);
+      refs.input.value = formData.email;
+      refs.textarea.value = formData.message;
+   }
+};
 

68a1bbc39d703ddcfda85a0438cca54853aaf136
leonid12z <lelik_ceh@ukr.net>
2022-02-16 11:59:23 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -7,33 +7,4 @@ import { galleryItems } from "./gallery-items";
 // Change code below this line
 console.log(galleryItems);
 
-const galleryList = document.querySelector('.gallery');
-const galleryMarkup = elementGalleryMarkup(galleryItems);
 
-galleryList.insertAdjacentHTML('beforeend', galleryMarkup);
-galleryList.addEventListener('click', onGalleryClick);
-
-
-function elementGalleryMarkup(items) {
-   return galleryItems.map(({ preview, original, description }) => {
-
-      return `<a class = "gallery__item" href = "${original}">
-<img class = "gallery__image" src = "${preview}" alt = "${description}"/>
-</a>`
-   }).join('');
-};
-
-function onGalleryClick(sample) {
-   sample.preventDefault();
-
-   if (sample.target.nodeName !== 'IMG') {
-      return;
-   }
-   
-   let gallery =
-      new SimpleLightbox('.gallery a',
-         {
-            captionsData: 'alt',
-            captionDelay: 250,
-         });
-};

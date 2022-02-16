1de2d1807b48571f77578d526a596fce5070909a
leonid12z <lelik_ceh@ukr.net>
2022-02-16 20:18:18 +0200

    add-files-08d/z

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,10 +1,41 @@
-// Add imports above this line
 // Описан в документации
 import SimpleLightbox from "simplelightbox";
 // Дополнительный импорт стилей
 import "simplelightbox/dist/simple-lightbox.min.css";
+// Add imports above this line
 import { galleryItems } from "./gallery-items";
 // Change code below this line
 console.log(galleryItems);
+
+const galleryList = document.querySelector('.gallery');
+const galleryMarkup = elementGalleryMarkup(galleryItems);
+
+galleryList.insertAdjacentHTML('beforeend', galleryMarkup);
+galleryList.addEventListener('click', onGalleryClick);
+
+
+function elementGalleryMarkup(items) {
+   return galleryItems.map(({ preview, original, description }) => {
+
+      return `<a class = "gallery__item" href = "${original}">
+<img class = "gallery__image" src = "${preview}" alt = "${description}"/>
+</a>`
+   }).join('');
+};
+
+function onGalleryClick(sample) {
+   sample.preventDefault();
+
+   if (sample.target.nodeName !== 'IMG') {
+      return;
+   }
+   
+   let gallery =
+      new SimpleLightbox('.gallery a',
+         {
+            captionsData: 'alt',
+            captionDelay: 250,
+         });
+};
 
 
